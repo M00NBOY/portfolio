3e81e71f1b8df19fbea9a046342d3e0d06035c68
Mahel <zeroualmahel@gmail.com>
2018-11-08 11:38:26 +0100

    add/reset add/font add/Square composent

---
+++ src/components/Square.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="square">
+    hello
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Square',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+</style>
